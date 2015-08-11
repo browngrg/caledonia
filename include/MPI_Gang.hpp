563b2aa44122ca8757f4a9042e2a59d4db0e78a8
browngrg <browngrg@comcast.net>
2015-08-11 09:03:14 -0400

    Refactored MPI_Gang with MPI_Struct

--- include/MPI_Gang.hpp
+++ include/MPI_Gang.hpp
@@ -8,6 +8,68 @@
 #include<mpi.h>
 #endif
 
+struct MPI_Struct
+{
+public:
+   int iproc,nproc;
+#  ifdef USE_MPI
+   MPI_Group group;
+   MPI_Comm comm;
+#  else
+   int group;
+   int comm;
+#  endif
+public:
+
+   MPI_Struct() 
+   { 
+      iproc=0; 
+      nproc=1; 
+#     ifdef USE_MPI
+      group = MPI_GROUP_NULL;
+      comm = MPI_COMM_NULL;
+#     else
+      group = 0;
+      comm = 0;
+#     endif
+   }
+
+   void copy(const MPI_Struct& s) 
+   { 
+      iproc=s.iproc;
+      nproc=s.nproc;
+      group=s.group;
+      comm =s.comm;
+   }
+
+   void operator=(const MPI_Struct& s) { this->copy(s); }
+
+   bool in() const 
+   { 
+#  ifdef USE_MPI
+      return comm!=MPI_COMM_NULL; 
+#  else
+      return true;
+#  endif
+   }
+
+
+   static MPI_Struct world() 
+   {
+      MPI_Struct tmp;
+#     ifdef USE_MPI
+      tmp.comm = MPI_COMM_WORLD;
+      MPI_Comm_rank(tmp.comm,&tmp.iproc);
+      MPI_Comm_size(tmp.comm,&tmp.nproc);
+#     else
+      tmp.iproc = 0;
+      tmp.nproc = 1;
+#     endif
+      return tmp;
+   }
+
+};
+
 
 // Break a pool of MPI processes into Gangs that work together
 struct MPI_Gang
@@ -17,34 +79,18 @@ private:
    enum  { verbose_debug = false };          // very verbose tracing
 
 public:
-   int igang,ngang;
-   int iproc_pool, nproc_pool;
-   int iproc_gang, nproc_gang;
-#  ifdef USE_MPI
+
+   int igang, ngang;
+   MPI_Struct pool, gang, lead;
    bool owner;
-   MPI_Group group_pool, group_gang, group_lead;
-   MPI_Comm  comm_pool, comm_gang, comm_lead;
-#  endif
 
 public:
 
    MPI_Gang() 
    { 
-      igang      = 0; 
-      ngang      = 1; 
-      iproc_pool = 0; 
-      nproc_pool = 1; 
-      iproc_gang = 0;
-      nproc_gang = 1;
-#     ifdef USE_MPI
+      igang = 0; 
+      ngang = 1; 
       owner = false;
-      group_pool = MPI_GROUP_NULL;
-      group_gang = MPI_GROUP_NULL;
-      group_lead = MPI_GROUP_NULL;
-      comm_pool  = MPI_COMM_NULL;
-      comm_gang  = MPI_COMM_NULL;
-      comm_lead  = MPI_COMM_NULL;
-#     endif
    }
 
    MPI_Gang(const MPI_Gang& orig) { this->copy(orig); }
@@ -56,12 +102,12 @@ public:
       int final_flag;
       MPI_Finalized(&final_flag);
       if( final_flag ) return;
-      if( group_pool!=MPI_GROUP_NULL ) MPI_Group_free(&group_pool);
-      if( group_gang!=MPI_GROUP_NULL ) MPI_Group_free(&group_gang);
-      if( group_lead!=MPI_GROUP_NULL ) MPI_Group_free(&group_lead);
-      if(  comm_pool!=MPI_COMM_NULL  ) MPI_Comm_free(&comm_pool);
-      if(  comm_gang!=MPI_COMM_NULL  ) MPI_Comm_free(&comm_gang);
-      if(  comm_lead!=MPI_COMM_NULL  ) MPI_Comm_free(&comm_lead);
+      if( pool.group!=MPI_GROUP_NULL ) MPI_Group_free(&pool.group);
+      if( gang.group!=MPI_GROUP_NULL ) MPI_Group_free(&gang.group);
+      if( lead.group!=MPI_GROUP_NULL ) MPI_Group_free(&lead.group);
+      if(  pool.comm!=MPI_COMM_NULL  ) MPI_Comm_free(&pool.comm);
+      if(  gang.comm!=MPI_COMM_NULL  ) MPI_Comm_free(&gang.comm);
+      if(  lead.comm!=MPI_COMM_NULL  ) MPI_Comm_free(&lead.comm);
 #     endif
    }
 
@@ -71,37 +117,27 @@ public:
    {
       igang = orig.igang;
       ngang = orig.ngang;
-      iproc_pool = orig.iproc_pool;
-      nproc_pool = orig.nproc_pool;
-      iproc_gang = orig.iproc_gang;
-      nproc_gang = orig.nproc_gang;
-#     ifdef USE_MPI
+      pool  = orig.pool;
+      gang  = orig.gang;
+      lead  = orig.lead;
       owner = false;
-      group_pool = orig.group_pool;
-      group_gang = orig.group_gang;
-      group_lead = orig.group_lead;
-      comm_pool  = orig.comm_pool;
-      comm_gang  = orig.comm_gang;
-      comm_lead  = orig.comm_lead;
+   }
+
+   
+   void set_pool(const MPI_Struct& _pool)
+   {
+      pool.copy(_pool);
+#     ifdef USE_MPI
+      if(pool.in()) MPI_Comm_dup(_pool.comm,&pool.comm);
 #     endif
    }
 
 
-   // Methods that set the pool of workers can
-   // only be defined if MPI is available because
-   // they need MPI types
-#  ifdef USE_MPI
-   void set_pool(const MPI_Comm& comm)
+   void init(int NGang, const MPI_Struct& _pool)
    {
-      MPI_Comm_dup(comm,&comm_pool);
-   }
-
-   void init(int NGang, const MPI_Comm& comm)
-   {
-      this->set_pool(comm);
+      this->set_pool(_pool);
       this->init(NGang);
    }
-#  endif
 
 
    void init(int NGang=1)
@@ -112,68 +148,82 @@ public:
 #     else
       owner = true;
       ngang = NGang;
-      if(comm_pool==MPI_COMM_NULL) this->set_pool(MPI_COMM_WORLD);
+      //if(pool.comm==MPI_COMM_NULL) this->set_pool(MPI_Struct::world());
       if(verbose_debug) std::cout << __FILE__ << ":" << __LINE__ << " NGang=" << ngang << std::endl;
       // Get information about the pool of processes
-      MPI_Comm_rank(comm_pool,&iproc_pool);
-      MPI_Comm_size(comm_pool,&nproc_pool);
-      MPI_Comm_group(comm_pool,&group_pool);
+      if( !pool.in() ) return;
+      MPI_Comm_rank(pool.comm, &pool.iproc);
+      MPI_Comm_size(pool.comm, &pool.nproc);
+      MPI_Comm_group(pool.comm,&pool.group);
       // Create a communicator for each Gang of workers
-      int NPerGang   = nproc_pool/ngang;
+      int NPerGang   = pool.nproc/ngang;
       if( NPerGang<1 ) 
       {
          NPerGang = 1;
-         ngang    = nproc_pool;
+         ngang    = pool.nproc;
       }
-      if( (nproc_pool % NPerGang) != 0 )
+      if( (pool.nproc % NPerGang) != 0 )
       {
-         if( iproc_pool==0 ) 
+         if( pool.iproc==0 ) 
             std::cout << __FILE__ << ":" << __LINE__ << "Can't evenly divide processes into gangs" << std::endl
-                      << "ngang=" << ngang << " npool=" << nproc_pool << std::endl;
+                      << "ngang=" << ngang << " npool=" << pool.nproc << std::endl;
       }
-      igang = iproc_pool/NPerGang;
+      igang = pool.iproc/NPerGang;
       int gang_range[ngang][3];
       for(int ig=0; ig<ngang; ig++)
       {
          gang_range[ig][0] = ig*NPerGang;           // First process in gang
          gang_range[ig][1] = (ig+1)*NPerGang-1;     // Last process in gang
          gang_range[ig][2] = 1;                     // Stride through original group
-         if(gang_range[ig][1]>=nproc_pool)
-            gang_range[ig][1] = nproc_pool-1;
+         if(gang_range[ig][1]>=pool.nproc)
+            gang_range[ig][1] = pool.nproc-1;
       }
-      if(verbose_debug && iproc_pool==0)
+      if(verbose_debug && pool.iproc==0)
       {
          std::cout << "range =";
          for(int i=0; i<(ngang*3); i++) std::cout << " " << gang_range[igang][i];
          std::cout << std::endl;
       }
-      MPI_Group_range_incl(group_pool,1,&(gang_range[igang]),&group_gang);
-      MPI_Comm_create(comm_pool,group_gang,&comm_gang);
-      if( comm_gang!=MPI_COMM_NULL )
+      if(pool.in()) 
       {
-         MPI_Comm_rank(comm_gang,&iproc_gang);
-         MPI_Comm_size(comm_gang,&nproc_gang);
+         MPI_Group_range_incl(pool.group,1,&(gang_range[igang]),&gang.group);
+         MPI_Comm_create(pool.comm,gang.group,&gang.comm);
+         if( gang.in() )
+         {
+            MPI_Comm_rank(gang.comm,&gang.iproc);
+            MPI_Comm_size(gang.comm,&gang.nproc);
+         }
       }
       else
       {
-         std::cout << "iproc=" << iproc_pool << " not assigned to a gang" << std::endl;
+         std::cout << "iproc=" << pool.iproc << " not assigned to a gang" << std::endl;
          return;
       }
-      if(verbose_debug) std::cout << __FILE__ << ":" << __LINE__ << " iproc=" << iproc_pool << " igang=" << igang << "/" << ngang 
-                                  << " iproc_gang=" << iproc_gang << "/" << nproc_gang << std::endl;
+      if(verbose_debug) std::cout << __FILE__ << ":" << __LINE__ << " iproc=" << pool.iproc << " igang=" << igang << "/" << ngang 
+                                  << " iproc_gang=" << gang.iproc << "/" << gang.nproc << std::endl;
       // Create a communicator for lead-processes
       std::vector<int> lead_rank(ngang);
       for(int i=0; i<ngang; i++)
          lead_rank[i] = i*NPerGang;
-      MPI_Group_incl(group_pool,ngang,&(lead_rank[0]),&group_lead);
-      MPI_Comm_create(comm_pool,group_lead,&comm_lead);
-      if( iproc_gang==0 )
+      if( pool.in() ) 
+      {
+         MPI_Group_incl(pool.group,ngang,&(lead_rank[0]),&lead.group);
+         MPI_Comm_create(pool.comm,lead.group,&lead.comm);
+         if( lead.in() )
+         {
+            MPI_Comm_rank(lead.comm,&lead.iproc);
+            MPI_Comm_size(lead.comm,&lead.nproc);
+         }
+      }
+      if( gang.in() && gang.iproc==0 && !lead.in() )   // This is a paranoid check
+         std::cout << __FILE__ << ":" << __LINE__ << "(" << pool.iproc << ") gang.iproc=" << gang.iproc << std::endl;
+      if( lead.in() )
       {
          int ilead = -1;
-         MPI_Comm_rank(comm_lead,&ilead);
+         MPI_Comm_rank(lead.comm,&ilead);
          if( ilead!=igang ) 
          {
-            std::cout << "ilead!=igang for iproc=" << iproc_pool << " ilead=" << ilead << " igang=" << igang << " iproc_gang=" << iproc_gang << std::endl;
+            std::cout << "ilead!=igang for iproc=" << pool.iproc << " ilead=" << ilead << " igang=" << igang << " iproc_gang=" << gang.iproc << std::endl;
          }
       }
 #     endif
--- test/MPI_Gang.cpp
+++ test/MPI_Gang.cpp
@@ -5,32 +5,47 @@
 #include<iostream>
 
 
-void test1(const MPI_Comm& pool)
+
+void test1(const MPI_Struct& pool)
 {
    MPI_Gang mp;
    mp.init(5,pool);    // split processes into 5 gangs
 
-   if(mp.iproc_pool==0) std::cout << "init successful" << std::endl;
+   if(mp.pool.iproc==0) std::cout << "init successful" << std::endl;
 
    int ival = 0;
-   for(int i=0; i<5; i++) ival += i*mp.nproc_gang;   // 10*mp.nproc_gang == 2*mp.nproc_pool
+   for(int i=0; i<5; i++) ival += i*mp.gang.nproc;   // 10*mp.nproc_gang == 2*mp.nproc_pool
 
    // Sum over just leaders
    int isum = 0;
-   if(mp.comm_lead!=MPI_COMM_NULL) MPI_Reduce( &(mp.iproc_pool), &(isum), 1, MPI_INT, MPI_SUM, 0, mp.comm_lead );
-   if( mp.iproc_pool==0 )
+   if( mp.lead.in() ) MPI_Reduce( &(mp.pool.iproc), &(isum), 1, MPI_INT, MPI_SUM, 0, mp.lead.comm );
+   if( mp.pool.in() && mp.pool.iproc==0 )
       std::cout << "sum of leader ids = " << isum << std::endl;
 
    // Sum over all processes
-   MPI_Reduce( &(mp.iproc_pool), &(isum), 1, MPI_INT, MPI_SUM, 0, mp.comm_pool );
-   if( mp.iproc_pool==0  )
+   if( mp.pool.in() ) MPI_Reduce( &(mp.pool.iproc), &(isum), 1, MPI_INT, MPI_SUM, 0, mp.pool.comm );
+   if( mp.pool.in() && mp.pool.iproc==0  )
       std::cout << "sum of all ids = " << isum << std::endl;
 
    // Every processor get result from sum over leaders
-   if(mp.comm_lead!=MPI_COMM_NULL) MPI_Allreduce( &(mp.iproc_pool), &(isum), 1, MPI_INT, MPI_SUM, mp.comm_lead );
-   MPI_Bcast(&(isum),1,MPI_INT,0,mp.comm_gang);
+   if( mp.lead.in() ) MPI_Allreduce( &(mp.pool.iproc), &(isum), 1, MPI_INT, MPI_SUM, mp.lead.comm );
+   if( mp.gang.in() ) MPI_Bcast(&(isum),1,MPI_INT,0,mp.gang.comm);
    if( isum!=ival )
-      std::cout << " Wrong result from Bcast iproc=" << mp.iproc_pool << " isum=" << isum << " expected=" << ival << std::endl;
+      std::cout << " Wrong result from Bcast iproc=" << mp.pool.iproc << " isum=" << isum << " expected=" << ival << std::endl;
+}
+
+
+void test2(const MPI_Struct& pool)
+{
+   MPI_Gang bigger;
+   bigger.init(10,pool);
+   if( bigger.lead.in() && bigger.lead.nproc<5 && bigger.lead.iproc==0 )
+   {
+      std::cout << __FILE__ << ":" << __LINE__ << "(" << bigger.pool.iproc << ") " << bigger.lead.nproc << std::endl;
+      return;
+   }
+   MPI_Barrier(pool.comm);
+   if( bigger.lead.in() ) test1(bigger.lead);
 }
 
 
@@ -38,7 +53,8 @@ int main(int argc, char* argv[])
 {
 
    MPI_Init(&argc,&argv);
-   test1(MPI_COMM_WORLD);
+   test1(MPI_Struct::world());
+   test2(MPI_Struct::world());
    MPI_Finalize();
 
 }
