9a38e90442f6a4d60b53ee3a04b3f80cf707a561
browngrg <browngrg@comcast.net>
2015-08-11 14:34:10 -0400

    Separated MPI_Gang and MPI_Struct

---
+++ include/MPI_Struct.hpp
@@ -0,0 +1,71 @@
+#ifndef MPI_STRUCT_HPP_
+#define MPI_STRUCT_HPP_
+
+#ifdef USE_MPI
+#include<mpi.h>
+#endif
+
+// Combine the basic MPI info into one structure
+struct MPI_Struct
+{
+public:
+   int iproc,nproc;
+#  ifdef USE_MPI
+   MPI_Group group;
+   MPI_Comm comm;
+#  else
+   int group;
+   int comm;
+#  endif
+public:
+
+   MPI_Struct() 
+   { 
+      iproc=0; 
+      nproc=1; 
+#     ifdef USE_MPI
+      group = MPI_GROUP_NULL;
+      comm = MPI_COMM_NULL;
+#     else
+      group = 0;
+      comm = 0;
+#     endif
+   }
+
+   void copy(const MPI_Struct& s) 
+   { 
+      iproc=s.iproc;
+      nproc=s.nproc;
+      group=s.group;
+      comm =s.comm;
+   }
+
+   void operator=(const MPI_Struct& s) { this->copy(s); }
+
+   bool in() const 
+   { 
+#  ifdef USE_MPI
+      return comm!=MPI_COMM_NULL; 
+#  else
+      return true;
+#  endif
+   }
+
+
+   static MPI_Struct world() 
+   {
+      MPI_Struct tmp;
+#     ifdef USE_MPI
+      tmp.comm = MPI_COMM_WORLD;
+      MPI_Comm_rank(tmp.comm,&tmp.iproc);
+      MPI_Comm_size(tmp.comm,&tmp.nproc);
+#     else
+      tmp.iproc = 0;
+      tmp.nproc = 1;
+#     endif
+      return tmp;
+   }
+
+};
+
+#endif
