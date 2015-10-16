39a22a243266a968372bf781b64ce142b9aaca56
browngrg <browngrg@comcast.net>
2015-10-16 13:55:37 -0400

    before merge into main

--- mfia/MfiaWLfixed.cpp
+++ mfia/MfiaWLfixed.cpp
@@ -44,12 +44,10 @@ void DoIt(int& argc, char* argv[])
    MC_WangLandau wanglandau;
    wanglandau.wleta = 0;
    wanglandau.wlgamma_start = 1;
-   wanglandau.Qquit = 0.001;
+   wanglandau.Qquit = 0.05;
    wanglandau.MaxUpdate = 15;
    wanglandau.NWindow = 1;
-   //options.add_option( "numwin",  "number of windows",              ' ', &(wanglandau.NWindow));
    options.add_option( "numwalk", "number of walkers per window",   ' ', &(wanglandau.NWalkPerProcess));
-   //options.add_option( "overlap", "fractional overlap of windows",  ' ', &(wanglandau.fwinover));
    options.add_option( "nchunk",  "number of steps per iteration",  ' ', &(wanglandau.NChunk));
    options.add_option( "maxupdate","maximum number of iterations",  ' ', &(wanglandau.MaxUpdate));
    options.add_option( "wleta",    "weighting between WL and ITTM", ' ', &(wanglandau.wleta));
@@ -58,10 +56,8 @@ void DoIt(int& argc, char* argv[])
    wanglandau.LinearInterp = 0;
 
    // Local options
-   int ThetaBlo = 2;
-   int ThetaBhi= (64*64/4);
-   options.add_option( "ThetaBlo", "Lowest value of ThetaB for run",  ' ', &(ThetaBlo));
-   options.add_option( "ThetaBhi", "Highest value of ThetaB for run", ' ', &(ThetaBhi));
+   int NRes = 32;
+   options.add_option("res", "Number sampling pts in NA,NB", ' ', &(NRes));
 
    // Get parameters
    options.parse_command_line(argc,argv);
@@ -115,79 +111,109 @@ void DoIt(int& argc, char* argv[])
       nthread = world.nproc;
    }
 #  endif
+#  ifdef USE_CONDOR
+   sscanf(argv[1],"%d",&ithread);
+   sscanf(argv[2],"%d",&nthread);
+#  endif
 
-   char fname[100];
-   sprintf(fname,"MfiaWLfixedHist-%d.csv",ithread);
-   std::ofstream fout(fname);
-   fout << "# WL Histograms in the M-Phi plane" << std::endl;
-   fout << "# L=" << hamilton.L << " N=" << NGrid << " H=" << hamilton.H << " A=" << hamilton.A << std::endl;
-   fout << "# Column 1: N1A=ThetaA, upspins on A Sublattice" << std::endl;
-   fout << "# Column 2: N1B=ThetaB, upspins on B Sublattice" << std::endl;
-   fout << "# Column 3: M = -N + 2(ThetaA+ThetaB)" << std::endl;
-   fout << "# Column 4: Phi = 2(ThetaA-ThetaB)" << std::endl;
-   fout << "# Column 5: Neighbor Energy 2N-2U (U unlike bonds)" << std::endl;
-   fout << "# Column 6: Inferred Ungerade pairs (U=N-E/2)" << std::endl;
-   fout << "# Column 7: lnh, normalized to max=1" << std::endl;
-   fout << "# Column 8: lnh, normalized to sum=1" << std::endl;
+   std::vector< std::pair<int,int> > work;
+   char work_fn[100] = "MfiaWLfixed-todo.csv";
+   if( access(work_fn,F_OK)!=-1 )
+   {
+      std::ifstream fin(work_fn);
+      while( !fin.eof() )
+      {
+         std::string buffer;
+         int ThetaA,ThetaB;
+         fin >> ThetaA >> ThetaB;
+         if( !fin.eof() )
+         {
+            work.push_back( std::pair<int,int>(ThetaB,ThetaA ) );
+            std::getline(fin,buffer);
+         }
+      }
+   }
+   else 
+   {
+      int stride = NGrid/4/NRes;
+      for(int ThetaB=stride; ThetaB<=(NGrid/4); ThetaB+=stride)
+      {
+         for(int ThetaA=ThetaB; ThetaA<=(NGrid/4); ThetaA+=stride)
+         {
+            work.push_back( std::pair<int,int>(ThetaB,ThetaA ) );
+         }
+      }
+   }
+
    if(verbose) cout << __FILE__ << ":" << __LINE__ << " Create one walker" << endl;
    typedef WL_Walker<HAMILTONIAN::Config,HAMILTONIAN::Observables> Walker;
-   //wanglandau.mp_window.set_pool(MPI_Struct::world());
+
    wanglandau.mp_window.set_pool(MPI_Struct::local());
    std::vector<Walker> walkerpool(1);
    walkerpool[0].sigma.resize(NGrid);
-   if(ThetaBlo<2) ThetaBlo=2;
-   if(ThetaBhi<(NGrid/4)) ThetaBhi=NGrid/4;
-   for(int ThetaB=ThetaBlo; ThetaB<=ThetaBhi; ThetaB+=1)
+   for(int iwork=ithread; iwork<work.size(); iwork+=nthread)
    {
-      for(int ThetaA=ThetaB+ithread; ThetaA<=(NGrid/2-ThetaB); ThetaA+=nthread)
+      int ThetaB = work[iwork].first;
+      int ThetaA = work[iwork].second;
+      char fname[100];
+      sprintf(fname,"MfiaWLfixedHist-%03d-%03d.csv",ThetaB,ThetaA);
+      std::ofstream fout(fname);
+      fout << "# WL Histograms in the M-Phi plane" << std::endl;
+      fout << "# L=" << hamilton.L << " N=" << NGrid << " H=" << hamilton.H << " A=" << hamilton.A << std::endl;
+      fout << "# Column 1: N1A=ThetaA, upspins on A Sublattice" << std::endl;
+      fout << "# Column 2: N1B=ThetaB, upspins on B Sublattice" << std::endl;
+      fout << "# Column 3: M = -N + 2(ThetaA+ThetaB)" << std::endl;
+      fout << "# Column 4: Phi = 2(ThetaA-ThetaB)" << std::endl;
+      fout << "# Column 5: Neighbor Energy 2N-2U (U unlike bonds)" << std::endl;
+      fout << "# Column 6: Inferred Ungerade pairs (U=N-E/2)" << std::endl;
+      fout << "# Column 7: lnh, normalized to max=1" << std::endl;
+      fout << "# Column 8: lnh, normalized to sum=1" << std::endl;
+      Walker& walker(walkerpool[0]);
+      std::fill(walker.sigma.begin(),walker.sigma.end(),-1);
+      int nflip=0;
+      for(int isite=0; isite<NGrid && nflip<ThetaA; isite++)
       {
-         Walker& walker(walkerpool[0]);
-         std::fill(walker.sigma.begin(),walker.sigma.end(),-1);
-         int nflip=0;
-         for(int isite=0; isite<NGrid && nflip<ThetaA; isite++)
+         if( hamilton.stagmask[isite]>0 )
          {
-            if( hamilton.stagmask[isite]>0 )
-            {
-               walker.sigma[isite]=+1;
-               nflip++;
-            }
+            walker.sigma[isite]=+1;
+            nflip++;
          }
-         nflip=0;
-         for(int isite=0; isite<NGrid && nflip<ThetaB; isite++)
+      }
+      nflip=0;
+      for(int isite=0; isite<NGrid && nflip<ThetaB; isite++)
+      {
+         if( hamilton.stagmask[isite]<0 )
          {
-            if( hamilton.stagmask[isite]<0 )
-            {
-               walker.sigma[isite]=+1;
-               nflip++;
-            }
+            walker.sigma[isite]=+1;
+            nflip++;
          }
-         hamilton.calc_observable(walker.sigma,walker.now);
-         std::cout << "M=" << walker.now.M << "=" << -NGrid+2*(ThetaA+ThetaB) << " P="  << walker.now.P << "=" <<  2*(ThetaA-ThetaB) << std::endl;
-         wanglandau.partition_windows(energy,lng_est);
-         wanglandau.init_pool(walkerpool);   
-         for(int iwalk=0; iwalk<walkerpool.size(); iwalk++)
-            walkerpool[iwalk].set_fixed(energy,lng_est);
-         wanglandau.DoConverge(hamilton,walkerpool,measure_obj);
-         // Save results to a master file
-         std::vector<double> Sadd( walker.S.size() );
-         for(int i=0; i<walker.window.NBin; i++) Sadd[i] = walker.S[i] + walker.Sfixed[i];
-         double Smax = -std::numeric_limits<double>::max();
-         for(int i=0; i<walker.window.NBin; i++) 
-            if( walker.h[i]>0 ) Smax = std::max(Smax,Sadd[i]);
-         for(int i=0; i<walker.window.NBin; i++) Sadd[i] -= Smax;
-         double hnorm = 0;
-         for(int i=0; i<walker.window.NBin; i++) 
-            if( walker.h[i]>0 ) hnorm += std::exp(Sadd[i]);
-         hnorm = std::log(hnorm);
-         for(int i=0; i<walker.window.NBin; i++)
+      }
+      hamilton.calc_observable(walker.sigma,walker.now);
+      std::cout << "M=" << walker.now.M << "=" << -NGrid+2*(ThetaA+ThetaB) << " P="  << walker.now.P << "=" <<  2*(ThetaA-ThetaB) << std::endl;
+      wanglandau.partition_windows(energy,lng_est);
+      wanglandau.init_pool(walkerpool);   
+      for(int iwalk=0; iwalk<walkerpool.size(); iwalk++)
+         walkerpool[iwalk].set_fixed(energy,lng_est);
+      wanglandau.DoConverge(hamilton,walkerpool,measure_obj);
+      // Save results to a master file
+      std::vector<double> Sadd( walker.S.size() );
+      for(int i=0; i<walker.window.NBin; i++) Sadd[i] = walker.S[i] + walker.Sfixed[i];
+      double Smax = -std::numeric_limits<double>::max();
+      for(int i=0; i<walker.window.NBin; i++) 
+         if( walker.h[i]>0 ) Smax = std::max(Smax,Sadd[i]);
+      for(int i=0; i<walker.window.NBin; i++) Sadd[i] -= Smax;
+      double hnorm = 0;
+      for(int i=0; i<walker.window.NBin; i++) 
+         if( walker.h[i]>0 ) hnorm += std::exp(Sadd[i]);
+      hnorm = std::log(hnorm);
+      for(int i=0; i<walker.window.NBin; i++)
+      {
+         if( walker.h[i]>0 )
          {
-            if( walker.h[i]>0 )
-            {
-               int E = static_cast<int>(walker.window.unbin(i));
-               int U = NGrid - E/2;
-               fout << ThetaA << " " << ThetaB << " " << walker.now.M << " " << walker.now.P << " " << E << " " << U << " " 
-                    << std::setprecision(12) <<  Sadd[i] << " " << std::setprecision(12) << Sadd[i]-hnorm << std::endl;
-            }
+            int E = static_cast<int>(walker.window.unbin(i));
+            int U = NGrid - E/2;
+            fout << ThetaA << " " << ThetaB << " " << walker.now.M << " " << walker.now.P << " " << E << " " << U << " " 
+                 << std::setprecision(12) <<  Sadd[i] << " " << std::setprecision(12) << Sadd[i]-hnorm << std::endl;
          }
       }
    }
